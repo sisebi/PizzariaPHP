adcacfce6f0dcddfd5f1288401e4c9fba913f31c
sisebi <qwem126@gmail.com>
2018-11-26 11:16:29 -0200

    Update PessoaFormList.php

--- app/control/pizzaria/pessoa/PessoaFormList.php
+++ app/control/pizzaria/pessoa/PessoaFormList.php
@@ -4,8 +4,8 @@ use Adianti\Base\TStandardFormList;
  * @Eliezer
  */
 class PessoaFormList extends TStandardFormList{
-    protected $form;     // registration form
-    protected $datagrid; // listing
+    protected $form;     
+    protected $datagrid; 
     protected $pageNavigation;
     protected $loaded;
     
@@ -13,8 +13,7 @@ class PessoaFormList extends TStandardFormList{
         parent::__construct();
         parent::setDatabase('db_sistema_erp');
         parent::setActiveRecord('PessoaModel');
-        
-        $author_id         = new TDBSeekButton('author_id', 'library', $this->form->getName(), 'Author', 'name', 'author_id', 'author_name');
+                
     }
 
     
